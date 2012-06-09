07aaf0125eb36203fb604aeae45f412ab9c2d3ee
snowmantw <snowmantw@gmail.com>
2012-06-09 23:45:11 +0800

    OOP based low-level implementation is done

--- library/main/arrow.js
+++ library/main/arrow.js
@@ -14,54 +14,58 @@ Notifier.trigger = function(note){
     Backbone.Events.trigger.call(Notifier, note.name, note);
 }
 
-Arrow = function(n_seq, p_seq){
-    _.each(n_seq, function(waits){ waits.__triggered = []; 
-                                   waits.__triggered.empty = function(){this.length = 0; } 
-    } );
 
-    this.n_seq = n_seq;
-    this.p_seq = p_seq;
-
-    // count down to zero if current triggered procs are all done.
-    this.ipc = 0; 
+Wait = function(name, proc){
+    this.name = name;
+    this.proc = proc;
 }
 
-// Note -> [ Note-> Note () ]
-Arrow.one2many = function(n, ps){
-    _.each(ps, function(p){
-        Notifier.trigger(p(n));
-    });
+Wait.prototype.match = function(note){
+    return note.name == this.name;
 }
 
-// [ Note ] -> [ Note-> Note () ]
-Arrow.many2many = function(ns, ps){
-
-    // maybe we can use other ways to optimize many2many.
-    _.each(ns, function(n){
-        Arrow.one2many(n,ps);
-    });
+Wait.prototype.execute = function(note){
+    return this.proc(note);
 }
 
-/* Should we have many2one ? Composed handler should handle many-many by itself.
- *
-// The original event triggering has no many2one machanism...
-Arrow.many2one = function(ns, p){
-    
-    Notifier.trigger(p({'name': Arrow.concat_notes(ns), 'notes': ns}));
+Waits = function(arr_wait){
+    this.waits = arr_wait;
+    this.notes_triggered = [];
+    this.is_or = false;
 }
 
-// [ Note ] -> Note
-Arrow.concat_notes = function(ns){
-    //return _.reduce(ns, function(memo_name, n){ return memo_name+'_'+n.name; }, "");
-}
-*/
-
-// ([String], [Note]) -> [Note] OR []
-Arrow.or = function(waits, triggers){
+//[Note] -> [Note]
+Waits.prototype.execute = function(notes){
 
     var result = [];
-    _.any( triggers, function(note) { 
-            if( _.any( waits, function(name_w){ return note.name == name_w }) )
+    _.each( this.waits, function(wait) {
+        _.each( notes, function(note){
+            result.push(wait.execute(note));
+        });
+    });
+
+    this.notes_triggered.length = 0;
+
+    return result;
+}
+
+// :: Note -> [Note] OR []
+Waits.prototype.match = function(note){
+
+    if( this.is_or ){ return this.match_or(note);  }
+    else            { return this.match_and(note); } 
+
+}
+
+// will return matched notes.
+// OR will get only one matched.
+// :: Note -> [Note] OR []
+Waits.prototype.match_or = function(note){
+
+    var result = [];
+
+    _.any( this.notes_triggered, function(note) { 
+            if( _.any( this.waits, function(wait){ return wait.match(node); } ))
             {
                 result.push(note);
                 return true;
@@ -71,132 +75,79 @@ Arrow.or = function(waits, triggers){
     return result;
 }
 
-// ([String], [Note]) -> [Note] OR []
-Arrow.and = function(waits, triggers){
+// :: Note -> [Note] OR []
+Waits.prototype.match_and = function(note){
 
     var result = [];
 
-    if( waits.length != triggers.length) { return result; }
+    if( this.waits.length != this.notes_triggered.length) { return result; }
     
     // if underscore optimized it's "difference" , this may improve performance.
-    if( 0 == _.difference(waits, _.pluck(triggers, 'name')).length )
+    if( 0 == _.difference(_.pluck(this.waits, 'name'), _.pluck(this.notes_triggered, 'name')).length )
     {
-        result = triggers;
+        result = this.notes_triggered;
     }
 
     return result;
 }
 
-// data of composed notes such as node_a AND node_b -> _a_b 
-//   will put in 'data' field and can be identified by index.
-//
-// note : {'name': String, 'data': [ {... node N's data} ] }
+Waits.prototype.trigger = function(note){
+    this.notes_triggered.push(note);
+}
 
-//first:: a b c -> a (b, d) (c, d)
-Arrow.prototype.first = function(arr){
-    
-    // return an new Arrow and_merge the pass-through callback and 'any' noty
-    return new Arrow( Arrow.and_merge( arr.n_seq,( ['all'] ) )
-                    , Arrow.and_merge( arr.p_seq,( [ function(note){
-                            return note;
-                      } ]))
-                    );   
+//[ Waits ]
+Arrow = function(w_seq){
+    this.w_seq = w_seq;
+    this.ipc = w_seq.length;
+}
+
+// Will called after every waits get done.
+Arrow.prototype.run = function(){
+
+    // all waits had been executed.
+    if( 0 == this.ipc ) { return; }
+
+    this.ipc --;
+
+    var waits = this.w_seq.shift();
+    this.bind(waits);
+
+    // push back to save all waits, and use [0] index to point out current waits.
+    this.w_seq.push(waits);
+}
+
+Arrow.prototype.reset = function(){
+
+    this.ipc = this.w_seq.length;
+}
+
+// rerun == reset; run;
+Arrow.prototype.rerun = function(){
+
+    this.reset();
+    this.run();
+}
+
+Arrow.prototype.bind = function(waits){
+
+    var THIS = this;
+    _.each( waits.waits , function(wait){
+        Notifier.on( wait.name, function(note){
+            waits.trigger(note);
+
+            // check if match or not.
+            var result_match = waits.match(note);
+            if( 0 != result_match.length ){
+                var notes_result = waits.execute(result_match);                
+                _.each(notes_result, function(note){Notifier.trigger(note);});
+                Notifier.off(wait.name);
+                THIS.run();
+            }
+        }); // Notifier.on
+    }); // _.each
 }
 
 //(>>>):: a b c -> a c d -> a b d
 Arrow.prototype.next = function(arr){
-    
-    return new Arrow( this.n_seq.concat( arr.n_seq ) ,
-                      this.p_seq.concat( arr.p_seq ) 
-                    );
+    return new Arrow( this.w_seq.concat(arr.w_seq) );
 }
-
-//(***):: a b c -> a b' c' -> a (b, b') (c, c') 
-Arrow.prototype.split = function(arr){
-    
-    return new Arrow( Arrow.and_merge(this.n_seq, arr.n_seq),
-                      Arrow.and_merge(this.p_seq, arr.p_seq));
-}
-
-
-// run ONCE: every triggered processors should check 
-//   if itself is the last one, and call bind function to bind next waits.
-Arrow.prototype.run = function(){
-    
-    if( 0 == this.n_seq.length ) { return ; }
-    this.current_waits =  this.n_seq.shift(); // get first notes and processors.
-    this.current_procs = this.p_seq.shift();
-    this.bind( this.current_waits, this.current_procs );
-}
-
-Arrow.prototype.wrap_p = function(p){
-    var THIS = this;
-
-    return function(){
-        var result = p.apply({},arguments); 
-        this.ipc --;
-
-        // shift out current waits and procs to make next run.
-        if( 0 == this.ipc) {
-            THIS.run();
-        }
-
-        return result;
-    };
-}
-
-// waits.__triggered is needed.
-Arrow.prototype.bind = function(waits, procs){
-
-    this.ipc = procs.length;
-
-    var THIS = this;
-
-    _.each(waits, function(wait){
-        Notifier.on(wait, function(note){
-
-            if( ! waits.__or ){ // is AND list.
-
-                waits.__triggered.push(note);   // Only AND need to save triggered notes.
-
-                // AND will trigger all events to all callback. 
-                // check if all events match the waits.
-                if( 0 != Arrow.and(waits, waits.__triggered ).length )
-                {
-                    // handle all events triggered.
-                    Arrow.many2many(waits.__triggered,_.map( procs, THIS.wrap_p, THIS) );
-                    waits.__triggered.empty();
-                }
-
-            } else { 
-                if( 0 != Arrow.or(waits, [note]).length )
-                {
-                    // OR will only trigger one event to all callback.
-                    Arrow.one2many(note, _.map( procs, THIS.wrap_p, THIS ) );
-                    waits.__triggered.empty();
-                }
-            }
-        });
-    });
-}
-
-Arrow.and_merge = function(s1, s2){
-    var zip = _.zip(s1, s2);
-
-    // zip will cause undefined if s1.length != s2.length .
-    var zip_noundef = [];
-    if( s1.length != s2.length ){
-        _.each(zip,function(es){
-           var noundef = _.reject(es, function(e){
-                return (e != undefined);
-           });
-
-           zip_noundef.push(noundef);
-        });
-
-        return _.flatten(zip_noundef);
-    } else {
-        return _.flatten(zip);
-    }
-}
-
